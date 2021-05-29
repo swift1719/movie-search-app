01b2d79912124fd11fb08ec84e7e2f899646489c
swift1719 <ayushpawar1501@gmail.com>
2021-05-30 00:15:16 +0530

    added header

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+	
+    <header > 🎥 Binge Search 🎥</header>
+            
+    )
+}
+
+export default Header
