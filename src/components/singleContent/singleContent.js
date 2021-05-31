aa66ad961b88b7bc855cd9c3869cbc9bfcc4b1db
swift1719 <ayushpawar1501@gmail.com>
2021-05-31 23:22:59 +0530

    added single card component

---
+++ src/components/singleContent/singleContent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const SingleContent = ({
+    id,
+    poster,
+    title,
+    date,
+    media_type,
+    vote_average
+}) => {
+    return (
+        <div>
+            {
+                title
+            }
+        </div>
+    )
+}
+
+export default SingleContent
--- src/pages/trending/trending.js
+++ src/pages/trending/trending.js
@@ -1,5 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';  
+import SingleContent from '../../components/singleContent/singleContent';
+
 const Trending = () => {
 
     const [content, setContent] = useState([]);
@@ -8,7 +10,7 @@ const Trending = () => {
         const {data}=await axios.get(
             `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
         );
-            // console.log(data);
+            // console.log(data.results);
         setContent(data.results);
     }
 
@@ -22,7 +24,16 @@ const Trending = () => {
             <div className="trending">
                 {
                     content && content.map(el => 
-                        console.log(el)
+                        //for movie title = el.title whereas for series title=el.name
+                        <SingleContent 
+                        key={el.id} 
+                        id={el.id} 
+                        poster={el.poster_path} 
+                        title={el.title || el.name} 
+                        date={el.first_air_date || el.release_date}
+                        media={el.media_type}
+                        vote_average={el.vote_average} 
+                        />
                     )
                 }
             </div>
