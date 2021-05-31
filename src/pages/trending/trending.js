549c9517255091e3454c5e4b1d8401182d4eea57
swift1719 <ayushpawar1501@gmail.com>
2021-05-31 23:12:25 +0530

    API connection established

--- src/pages/trending/trending.js
+++ src/pages/trending/trending.js
@@ -1,9 +1,31 @@
-import React from 'react';
-
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';  
 const Trending = () => {
+
+    const [content, setContent] = useState([]);
+
+    const fetchTrending= async ()=>{
+        const {data}=await axios.get(
+            `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+            // console.log(data);
+        setContent(data.results);
+    }
+
+    useEffect(() => {
+        fetchTrending();
+    }, []);
+
     return (
         <div>
             <span className="page-title">Trending</span>
+            <div className="trending">
+                {
+                    content && content.map(el => 
+                        console.log(el)
+                    )
+                }
+            </div>
         </div>
     )
 }
