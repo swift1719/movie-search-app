02887857dd6a0d3e770b48cc39d688f52ec51245
swift1719 <ayushpawar1501@gmail.com>
2021-06-09 18:26:40 +0530

    adding modal

--- src/components/singleContent/singleContent.js
+++ src/components/singleContent/singleContent.js
@@ -2,6 +2,7 @@ import { Badge } from '@material-ui/core';
 import React from 'react';
 import {img_300,unavailable} from '../../config/config';
 import './singleContent.css';
+import ContentModal from '../Modal/contentModal';
 
 const SingleContent = ({
         id,
@@ -12,7 +13,7 @@ const SingleContent = ({
         vote_average
     }) => {
     return (
-        <div className="media">
+        <ContentModal media_type={media_type} id={id} >
             <Badge 
             badgeContent={vote_average}
             color={vote_average>6?'primary':'secondary'}
@@ -27,7 +28,7 @@ const SingleContent = ({
                 {media_type==="TV"?"Web series":"Movie"}
                 <span className="subTitle">{date}</span>
             </span>
-        </div>
+        </ContentModal>
     )
 }
 
